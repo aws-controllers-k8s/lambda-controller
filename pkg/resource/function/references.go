9b182745656d8f7bbf3f0c865dc31e78670e64a1
Vandita2020 <vandita.vk20@gmail.com>
2022-10-17 13:51:58 -0700

    Add resource reference for  VPC.SecurityGroupIDs

--- pkg/resource/function/references.go
+++ pkg/resource/function/references.go
@@ -36,6 +36,9 @@ import (
 // +kubebuilder:rbac:groups=kms.services.k8s.aws,resources=keys,verbs=get;list
 // +kubebuilder:rbac:groups=kms.services.k8s.aws,resources=keys/status,verbs=get;list
 
+// +kubebuilder:rbac:groups=ec2.services.k8s.aws,resources=securitygroups,verbs=get;list
+// +kubebuilder:rbac:groups=ec2.services.k8s.aws,resources=securitygroups/status,verbs=get;list
+
 // +kubebuilder:rbac:groups=ec2.services.k8s.aws,resources=subnets,verbs=get;list
 // +kubebuilder:rbac:groups=ec2.services.k8s.aws,resources=subnets/status,verbs=get;list
 
@@ -56,6 +59,9 @@ func (rm *resourceManager) ResolveReferences(
 	err := validateReferenceFields(ko)
 	if err == nil {
 		err = resolveReferenceForKMSKeyARN(ctx, apiReader, namespace, ko)
+	}
+	if err == nil {
+		err = resolveReferenceForVPCConfig_SecurityGroupIDs(ctx, apiReader, namespace, ko)
 	}
 	if err == nil {
 		err = resolveReferenceForVPCConfig_SubnetIDs(ctx, apiReader, namespace, ko)
@@ -79,6 +85,11 @@ func validateReferenceFields(ko *svcapitypes.Function) error {
 		return ackerr.ResourceReferenceAndIDNotSupportedFor("KMSKeyARN", "KMSKeyRef")
 	}
 	if ko.Spec.VPCConfig != nil {
+		if ko.Spec.VPCConfig.SecurityGroupRefs != nil && ko.Spec.VPCConfig.SecurityGroupIDs != nil {
+			return ackerr.ResourceReferenceAndIDNotSupportedFor("VPCConfig.SecurityGroupIDs", "VPCConfig.SecurityGroupRefs")
+		}
+	}
+	if ko.Spec.VPCConfig != nil {
 		if ko.Spec.VPCConfig.SubnetRefs != nil && ko.Spec.VPCConfig.SubnetIDs != nil {
 			return ackerr.ResourceReferenceAndIDNotSupportedFor("VPCConfig.SubnetIDs", "VPCConfig.SubnetRefs")
 		}
@@ -89,7 +100,7 @@ func validateReferenceFields(ko *svcapitypes.Function) error {
 // hasNonNilReferences returns true if resource contains a reference to another
 // resource
 func hasNonNilReferences(ko *svcapitypes.Function) bool {
-	return false || (ko.Spec.KMSKeyRef != nil) || (ko.Spec.VPCConfig != nil && ko.Spec.VPCConfig.SubnetRefs != nil)
+	return false || (ko.Spec.KMSKeyRef != nil) || (ko.Spec.VPCConfig != nil && ko.Spec.VPCConfig.SecurityGroupRefs != nil) || (ko.Spec.VPCConfig != nil && ko.Spec.VPCConfig.SubnetRefs != nil)
 }
 
 // resolveReferenceForKMSKeyARN reads the resource referenced
@@ -145,6 +156,71 @@ func resolveReferenceForKMSKeyARN(
 		}
 		referencedValue := string(*obj.Status.ACKResourceMetadata.ARN)
 		ko.Spec.KMSKeyARN = &referencedValue
+	}
+	return nil
+}
+
+// resolveReferenceForVPCConfig_SecurityGroupIDs reads the resource referenced
+// from VPCConfig.SecurityGroupRefs field and sets the VPCConfig.SecurityGroupIDs
+// from referenced resource
+func resolveReferenceForVPCConfig_SecurityGroupIDs(
+	ctx context.Context,
+	apiReader client.Reader,
+	namespace string,
+	ko *svcapitypes.Function,
+) error {
+	if ko.Spec.VPCConfig == nil {
+		return nil
+	}
+
+	if ko.Spec.VPCConfig.SecurityGroupRefs != nil &&
+		len(ko.Spec.VPCConfig.SecurityGroupRefs) > 0 {
+		resolvedReferences := []*string{}
+		for _, arrw := range ko.Spec.VPCConfig.SecurityGroupRefs {
+			arr := arrw.From
+			if arr == nil || arr.Name == nil || *arr.Name == "" {
+				return fmt.Errorf("provided resource reference is nil or empty")
+			}
+			namespacedName := types.NamespacedName{
+				Namespace: namespace,
+				Name:      *arr.Name,
+			}
+			obj := ec2apitypes.SecurityGroup{}
+			err := apiReader.Get(ctx, namespacedName, &obj)
+			if err != nil {
+				return err
+			}
+			var refResourceSynced, refResourceTerminal bool
+			for _, cond := range obj.Status.Conditions {
+				if cond.Type == ackv1alpha1.ConditionTypeResourceSynced &&
+					cond.Status == corev1.ConditionTrue {
+					refResourceSynced = true
+				}
+				if cond.Type == ackv1alpha1.ConditionTypeTerminal &&
+					cond.Status == corev1.ConditionTrue {
+					refResourceTerminal = true
+				}
+			}
+			if refResourceTerminal {
+				return ackerr.ResourceReferenceTerminalFor(
+					"SecurityGroup",
+					namespace, *arr.Name)
+			}
+			if !refResourceSynced {
+				return ackerr.ResourceReferenceNotSyncedFor(
+					"SecurityGroup",
+					namespace, *arr.Name)
+			}
+			if obj.Status.ID == nil {
+				return ackerr.ResourceReferenceMissingTargetFieldFor(
+					"SecurityGroup",
+					namespace, *arr.Name,
+					"Status.ID")
+			}
+			referencedValue := string(*obj.Status.ID)
+			resolvedReferences = append(resolvedReferences, &referencedValue)
+		}
+		ko.Spec.VPCConfig.SecurityGroupIDs = resolvedReferences
 	}
 	return nil
 }
