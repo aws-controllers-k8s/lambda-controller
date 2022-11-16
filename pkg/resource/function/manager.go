48ce8032d4a84c9b57785a9369a885e9e44131e7
Vandita Patidar <vandita.vk20@gmail.com>
2022-11-16 14:09:56 -0800

    Add sync functionality to re-queue until the Status is Active (#65)
    Re-queue when function `State` is not `Active`
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/function/manager.go
+++ pkg/resource/function/manager.go
@@ -268,6 +268,14 @@ func (rm *resourceManager) IsSynced(ctx context.Context, res acktypes.AWSResourc
 		panic("resource manager's IsSynced() method received resource with nil CR object")
 	}
 
+	if r.ko.Status.State == nil {
+		return false, nil
+	}
+	stateCandidates := []string{"Active"}
+	if !ackutil.InStrings(*r.ko.Status.State, stateCandidates) {
+		return false, nil
+	}
+
 	return true, nil
 }
 
--- test/e2e/tests/test_function.py
+++ test/e2e/tests/test_function.py
@@ -420,3 +420,59 @@ class TestFunction:
 
         # Check Lambda function doesn't exist
         assert not lambda_validator.function_exists(resource_name)
+
+    def test_function_is_synced(self, lambda_client):
+        resource_name = random_suffix_name("lambda-function", 24)
+
+        resources = get_bootstrap_resources()
+        logging.debug(resources)
+
+        replacements = REPLACEMENT_VALUES.copy()
+        replacements["FUNCTION_NAME"] = resource_name
+        replacements["BUCKET_NAME"] = resources.FunctionsBucket.name
+        replacements["LAMBDA_ROLE"] = resources.BasicRole.arn
+        replacements["LAMBDA_FILE_NAME"] = LAMBDA_FUNCTION_FILE_ZIP
+        replacements["RESERVED_CONCURRENT_EXECUTIONS"] = "0"
+        replacements["CODE_SIGNING_CONFIG_ARN"] = ""
+        replacements["AWS_REGION"] = get_region()
+
+        # Load Lambda CR
+        resource_data = load_lambda_resource(
+            "function",
+            additional_replacements=replacements,
+        )
+        logging.debug(resource_data)
+
+        # Create k8s resource
+        ref = k8s.CustomResourceReference(
+            CRD_GROUP, CRD_VERSION, RESOURCE_PLURAL,
+            resource_name, namespace="default",
+        )
+        k8s.create_custom_resource(ref, resource_data)
+        cr = k8s.wait_resource_consumed_by_controller(ref)
+
+        assert cr is not None
+        assert k8s.get_resource_exists(ref)
+
+        time.sleep(CREATE_WAIT_AFTER_SECONDS*3)
+
+        cr = k8s.wait_resource_consumed_by_controller(ref)
+
+        lambda_validator = LambdaValidator(lambda_client)
+        # Check Lambda function exists
+        assert lambda_validator.function_exists(resource_name)
+
+        assert cr["status"]["state"] == "Active"
+
+        function = lambda_validator.get_function(resource_name)
+        assert function is not None
+        assert function["Configuration"]["State"] == "Active"
+
+        # Delete k8s resource
+        _, deleted = k8s.delete_custom_resource(ref)
+        assert deleted is True
+
+        time.sleep(DELETE_WAIT_AFTER_SECONDS)
+
+        # Check Lambda function doesn't exist
+        assert not lambda_validator.function_exists(resource_name)
