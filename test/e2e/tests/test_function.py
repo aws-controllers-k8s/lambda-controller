8bf558b8a1433012ce3e508a6a4177c18acbd8b0
Vandita2020 <vandita.vk20@gmail.com>
2023-01-25 15:52:47 -0800

    Adding ephemeralStorage update and e2e tests for the update

--- test/e2e/tests/test_function.py
+++ test/e2e/tests/test_function.py
@@ -332,7 +332,7 @@ class TestFunction:
         assert lambda_validator.function_exists(resource_name)
 
         cr["spec"]["timeout"] = 10
-        cr["spec"]["ephemeralStorage"] = { "size" : 512 }
+        cr["spec"]["ephemeralStorage"] = { "size" : 1024 }
 
         # Patch k8s resource
         k8s.patch_custom_resource(ref, cr)
@@ -341,7 +341,7 @@ class TestFunction:
         # Check function updated fields
         function = lambda_validator.get_function(resource_name)
         assert function["Configuration"]["Timeout"] == 10
-        assert function["Configuration"]["EphemeralStorage"]["Size"] == 512
+        assert function["Configuration"]["EphemeralStorage"]["Size"] == 1024
 
         # Delete k8s resource
         _, deleted = k8s.delete_custom_resource(ref)
