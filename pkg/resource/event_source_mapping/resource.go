6f1be1222ce743d1b6cdf8266f504cac78cc65e0
ci-robot <ci-robot@k8s.io>
2025-09-19 17:17:45 +0000

    Update to ACK runtime `v0.52.0`, code-generator `v0.52.0`

--- pkg/resource/event_source_mapping/resource.go
+++ pkg/resource/event_source_mapping/resource.go
@@ -97,11 +97,11 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 
 // PopulateResourceFromAnnotation populates the fields passed from adoption annotation
 func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) error {
-	tmp, ok := fields["uuid"]
+	primaryKey, ok := fields["uuid"]
 	if !ok {
 		return ackerrors.NewTerminalError(fmt.Errorf("required field missing: uuid"))
 	}
-	r.ko.Status.UUID = &tmp
+	r.ko.Status.UUID = &primaryKey
 
 	return nil
 }
