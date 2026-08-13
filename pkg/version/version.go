cabef79c3d4c6564c7148d87fe3f5b6d549e1199
knottnt <trevor.knottnerus@gmail.com>
2026-08-13 16:39:35 -0700

    Use minor version bump

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -28,5 +28,5 @@ var (
 // time and are immutable for the life of the generated code.
 const (
 	ACKGenerateVersion   = "v0.62.1"
-	ACKGenerateBuildDate = "2026-08-13T01:03:22Z"
+	ACKGenerateBuildDate = "2026-08-13T23:38:16Z"
 )
